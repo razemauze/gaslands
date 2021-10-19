47670ff1336425f95950e641ff004bca0d4d91d2
Rasmus Bihl Larsen <hello@rasmusbihllarsen.com>
2021-10-19 13:26:46 +0200

    Init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a9f4b726674b1670a011ddc731271d19'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '77adccdd-c655-43ca-b587-17123c55edb7'
+};
